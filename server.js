b963e1f2e12e459cca10364b7e7fd9ba2802fdcf
Ayushkumar890 <ayushkumar.hp890@gmail.com>
2024-01-19 23:50:15 +0530

    server.js updated

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ app.set("views", __dirname + "/views/");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-//configuring database
 const port = "http://localhost:3000"
 
 app.use("/",(req,res)=>{
