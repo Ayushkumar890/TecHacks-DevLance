530715cd95a3e8f4ad3c767a1cd6976d11932fa9
Ayushkumar890 <ayushkumar.hp890@gmail.com>
2024-01-19 20:06:15 +0530

    structure created

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello ");
+});
+
+app.listen(3000, () => {
+  console.log(`server run at port http://localhost:3000`);
+});
