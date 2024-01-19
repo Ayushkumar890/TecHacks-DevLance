60b13e02e10fea716529c21d366e1145e3f89f10
Ayushkumar890 <ayushkumar.hp890@gmail.com>
2024-01-19 21:49:10 +0530

    server modified

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello ");
+app.get("/",(req,res)=>{
+    res.send("hello ");
 });
 
-app.listen(3000, () => {
-  console.log(`server run at port http://localhost:3000`);
-});
+app.listen(3000,()=>{
+    console.log(`server run at port http://localhost:3000`)
+})
