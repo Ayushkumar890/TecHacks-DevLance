5a4b029e61fa61042040cb017ab261665b10372d
AstitvaRajSinha <astitvaraj870@gmail.com>
2024-01-19 23:34:45 +0530

    index.ejs updated

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(express.static(__dirname + "/Public"));
+app.set("views", __dirname + "/views/");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("hello ");
-});
+
+//configuring database
+const port = "http://localhost:3000"
+
+app.use("/",(req,res)=>{
+  res.render("index");
+})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server ready at ${port}`);
