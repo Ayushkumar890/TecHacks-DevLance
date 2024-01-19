7aee7af1b58e64383607cc17e63662327e6e3e39
Ayushkumar890 <ayushkumar.hp890@gmail.com>
2024-01-19 23:05:07 +0530

    file structure updated

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(express.static(__dirname + "/Public"));
+app.set("views", __dirname + "/views/");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("hello ");
-});
 
-app.listen(3000,()=>{
-    console.log(`server run at port http://localhost:3000`)
+//configuring database
+const port = "http://localhost:3000"
+
+app.use("/",(req,res)=>{
+  res.render("index");
 })
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server ready at ${port}`);
+});
